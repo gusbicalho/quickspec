450d230fea8d76e56f0511e0b4e4e73b8b69009c
Gustavo Bicalho <gusbicalho@gmail.com>
2022-05-23 23:53:04 -0300

    remove bad instance for Arity

--- src/QuickSpec/Internal/Arity.hs
+++ src/QuickSpec/Internal/Arity.hs
@@ -6,10 +6,6 @@ class Arity f where
   -- | Measure the arity.
   arity :: f -> Int
 
-instance Arity Constant where
-  arity Minimal = 0
-  arity Constant{..} = con_arity
-
 instance (Labelled f, Arity f) => Arity (Fun f) where
   arity = arity . fun_value
 
